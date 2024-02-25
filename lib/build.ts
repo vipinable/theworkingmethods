7155876523b9bcd286904529cade345bde5ad982
vipinable <vipinable@gmail.com>
2024-02-25 06:27:22 -0500

    Initial commit

---
+++ lib/build.ts
@@ -0,0 +1,69 @@
+import { Stack, StackProps, Duration, RemovalPolicy, CfnOutput, Token, Lazy, EncodingOptions, Fn } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'; 
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+export class MainStack extends Stack {
+  public fnUrl: string
+
+  //BeginStackDefinition
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    console.log('accessing context 👉', this.node.tryGetContext('fromApp'));
+
+    // //Lambda layer creation definition
+    // const layer0 = new lambda.LayerVersion(this, 'LayerVersion', {
+    //   compatibleRuntimes: [
+    //     lambda.Runtime.PYTHON_3_6,
+    //     lambda.Runtime.PYTHON_3_7,
+    //     lambda.Runtime.PYTHON_3_8,
+    //   ],
+    //   code: lambda.Code.fromAsset(path.join(__dirname,'../../layer/bin')),
+    //   });
+
+    const s3Bucket = new s3.Bucket(this, 'healthcheck', {
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: false,
+    });
+
+    s3Bucket.grantRead(new iam.AccountRootPrincipal());
+    s3Bucket.grantPut(new iam.AccountRootPrincipal());
+          
+    //Index function definition
+    const healthcheckfn = new lambda.Function(this, 'healthcheckfn', {
+      description: 'healthcheck function',
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: 'main.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../src')),
+      // layers: [layer0],
+      environment: {
+        APPNAME: process.env.ApplicationName!,
+        ENVNAME: process.env.Environment!, 
+      },
+      });
+    
+      indexfn.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [
+        s3Bucket.arnForObjects("*"),
+        s3Bucket.bucketArn
+      ],
+      actions: [
+        's3:PutObject',
+        's3:GetObject',
+        's3:ListBucket'
+      ],
+      }));
+
+  //EndStack
+  }}
